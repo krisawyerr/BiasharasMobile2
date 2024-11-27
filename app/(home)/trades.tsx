2ab411328bf3791869d5e6dbfcdf1ea1c2e2a554
krisawyerr <Krisawyerrr@gmail.com>
2024-11-27 11:54:54 -0800

    finished list ui

--- app/(home)/trades.tsx
+++ app/(home)/trades.tsx
@@ -1,6 +1,7 @@
 import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import TRADES from "../../data/trades.json";
 import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Trade {
   transactionId: number;
@@ -43,17 +44,25 @@ export default function Trades() {
     <ScrollView style={styles.page}>
       {Object.entries(groupedTrades).map(([month, trades]) => (
         <View key={month} style={styles.section}>
-          <Text style={styles.text}>{month}</Text>
-          <FlatList
-            data={trades}
-            keyExtractor={(item) => item.transactionId.toString()}
-            renderItem={({ item }) => (
-              <View style={styles.tradeItem}>
-                <Text style={styles.text}>{item.type}</Text>
-                <Text style={styles.text}>{item.date}</Text>
+          <Text style={styles.header}>{month}</Text>
+          {trades.map(trade => (
+            <View style={styles.tradeItem} key={trade.transactionId}>
+              <View style={styles.dateGridCell}>
+                <Text style={styles.date}>{new Date(trade.date).getUTCDate()}</Text>
+                <Text style={styles.day}>{new Date(trade.date).toLocaleString('en-US', { weekday: 'short' })}</Text>
               </View>
-            )}
-          />
+              <View style={styles.gridCell}>
+                <Text style={styles.type}>{trade.type.toUpperCase()}</Text>
+                <Text style={styles.date}>{trade.currencyPair}</Text>
+              </View>
+              <View style={styles.pnlGridCell}>
+                <View style={styles.pnlContainer}>
+                  <Ionicons name={trade.profit! < 0 ? "arrow-down" : "arrow-up"} size={18} color={trade.profit! < 0 ? "#ff2847" : "#04a205"} />
+                  <Text style={{ color: trade.profit! < 0 ? "#ff2847" : "#04a205"}}>{trade.profit}</Text>
+                </View>
+              </View>
+            </View>
+          ))}
         </View>
       ))}
     </ScrollView>
@@ -64,18 +73,62 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: "#120f14",
-    padding: 10,
+    padding: 15,
   },
   section: {
-    marginBottom: 20,
+    marginBottom: 15,
+    backgroundColor: "#2c2734",
+    padding: 15,
+    borderRadius: 10
   },
   tradeItem: {
     padding: 10,
-    backgroundColor: "#1c1b21",
-    marginVertical: 5,
-    borderRadius: 5,
+    backgroundColor: "#2c2734",
+    marginVertical: 0,
+    borderTopColor: "#cac4cd15",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    flexWrap: 'wrap',
+  },
+  dateGridCell: {
+    width: '10%',
+  },
+  pnlGridCell: {
+    width: '50%',
+  },
+  gridCell: {
+    width: "40%",
+    justifyContent: "center"
+  },
+  header: {
+    color: "#cac4cd",
+    fontSize: 17,
+    paddingBottom: 10,
+    textAlign: "center"
+  },
+  date: {
+    color: "#cac4cd",
+    fontSize: 17,
+    textAlign: "center"
+  },
+  type: {
+    color: "#cac4cd75",
+    textAlign: "center"
+  },
+  day: {
+    color: "#cac4cd",
+    textAlign: "center"
   },
   text: {
-    color: "white",
+    color: "#cac4cd",
   },
+  pnlContainer: {
+    flex: 1,
+    backgroundColor: "#dfffea",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    gap: 3
+  }
 });
